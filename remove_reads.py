54fe1ae4913a1ed154c25a17c00903c02b166ffc
Douglas Wu <wckdouglas@gmail.com>
2015-03-12 00:34:45 -0500

    changed message

--- remove_reads.py
+++ remove_reads.py
@@ -1,15 +1,14 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
 import sys
 
 
 if len(sys.argv) != 3:
-    sys.exit('usage: python remove_reads.py fastqFile idFile \n\
-              This is a prgram to remove records from id list\n\
-              output is a fastq file\n\
-                    1. sequence file needs to be fastq\n\
-                    2. idfile is one id per line text file (without "@" sign in front of the id )')
+    sys.exit('\nusage: python remove_reads.py fastqFile idFile \n\
+              \nThis is a prgram to remove records from id list output is a fastq file \
+              \n      1. sequence file needs to be fastq \
+              \n      2. idfile is one id per line text file (without "@" sign in front of the id )\n')
 
 # set up fastq index
 index = {id.split(' ')[0]:'@%s\n%s\n+\n%s' %(id,seq,qual) for id, seq, qual in FastqGeneralIterator(open(sys.argv[1]))}
