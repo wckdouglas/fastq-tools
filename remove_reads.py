3153f779d5116f3431c2325a63e61a9317b4b0cf
Douglas Wu <wckdouglas@gmail.com>
2015-03-12 00:21:39 -0500

    added script

---
+++ remove_reads.py
@@ -0,0 +1,28 @@
+#!/bin/env python
+
+from Bio.SeqIO.QualityIO import FastqGeneralIterator
+import fileinput,sys
+
+
+if len(sys.argv) != 3:
+    sys.exit('usage: python remove_reads.py fastqFile idFile \
+              \nThis is a prgram to remove records from id list\
+              \noutput is a fastq file\
+              \n      1. sequence file needs to be fastq\
+              \n      2. idfile is one id per line text file')
+
+# set up fastq index
+index = {id.split(' ')[0]:'@%s\n%s\n+\n%s' %(id,seq,qual) for id, seq, qual in FastqGeneralIterator(open(sys.argv[1]))}
+
+# read mapped ID
+mapped_id_list = [line.strip() for line in open(sys.argv[2],'r')]
+
+# get unmapped IDs
+ids = set(index.keys()) - set(mapped_id_list)
+ids = list(ids)
+ids.sort()
+
+# print sequences
+for id in ids:
+    print index[id]
+
